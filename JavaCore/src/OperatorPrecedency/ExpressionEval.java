ecfc4dd12dc099e434cfb8d27895352bf72cc315
Amaresh <Amaresh@Amaresh>
2022-06-21 23:15:09 +0530

    second commit

---
+++ JavaCore/src/OperatorPrecedency/ExpressionEval.java
@@ -0,0 +1,13 @@
+package OperatorPrecedency;
+
+public class ExpressionEval {
+
+	public static void main(String[] args) {
+		int a=20;
+		float s= ++a + a-- - --a + a++;
+		System.out.println(s);
+	
+
+	}
+
+}
