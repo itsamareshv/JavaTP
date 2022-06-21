98dddd35787de0f4cf8e604f904c2a71751b9f52
Amaresh <Amaresh@Amaresh>
2022-06-21 23:41:40 +0530

    control statements

--- JavaCore/src/Operators.java
+++ JavaCore/src/Operators.java
@@ -1,11 +1,25 @@
-
+/**
+ * 
+ * @author Amaresh V
+ *
+ */
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int a=1;
+		float b=2;
 		System.out.println("RESULT = "+(a+a));
-
+		/*
+		 * 
+		 */
+		System.out.println("logical operators = "+((a>b)&&(b>a)));//logical operands
+		
+		int time=13;
+		String s = (time>12)? "PM" : "AM";
+		System.out.println(s);//ternary operator
+		
+		
 	}
 
 }
---
+++ JavaCore/src/controlstatements/if_statement.java
@@ -0,0 +1,28 @@
+package controlstatements;
+
+public class if_statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20,b=20;
+		if(a>b)
+		{
+			System.out.println("A is greater");
+		}else
+		{
+			System.out.println("B is greater");
+		}
+		
+		
+		if(a+b==30) {
+			System.out.println(true);
+		}else if(a+b==20) {
+			
+			System.out.println(false);
+		}else {
+			System.out.println("Thank You Come Back Again");
+		}
+	}
+	}
+
+
